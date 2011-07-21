3d1d44440ed2414cb9a3bdbf9c4cd553eb78e3e0
noelboss <n.company@me.com>
2011-07-21 11:35:39 +0200

    Removed old files

--- Classes/Controller/PodcastController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class Tx_Podcast_Controller_PodcastController extends Tx_Extbase_MVC_Controller_ActionController {
-	public function ViewAction(){
-        $this->view->assign('Name', 'World');		
-	}
-}
-?>
