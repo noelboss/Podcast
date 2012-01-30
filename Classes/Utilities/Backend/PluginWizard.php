55f42c95bef54910e05dc7a32412ed4747060a0d
Noël Bossart <noel.bossart@namics.com>
2012-01-30 20:09:35 +0100

    TER Release 0.3.4
    - Added minimal Documentation
    - Fixed Plugin registration
    - Fixed UI issue with subtitle rendered as secondary filed and while required.
    - Removed unused files

--- Classes/Utilities/Backend/PluginWizard.php
+++ Classes/Utilities/Backend/PluginWizard.php
@@ -14,12 +14,13 @@ class Tx_Podcast_Utilities_PluginWizard {
 	 * @return	array		Modified input array, having the item for Podcast Plugin added.
 	 */
 	function proc($wizardItems)	{
-		return $wizardItems['plugins_tx_podcast_display'] = array(
+		$wizardItems['plugins_tx_podcast_display'] = array(
 			'icon'=>t3lib_extMgm::extRelPath('podcast').'Resources/Public/Icons/be_wizard.gif',
 			'title'=>Tx_Extbase_Utility_Localization::translate("backend.wizard", "podcast", NULL),
-			'description'=>Tx_Extbase_Utility_Localization::translate("backend.description", "podcast", NULL),
+			'description'=>Tx_Extbase_Utility_Localization::translate("backend.wizard.description", "podcast", NULL),
 			'params'=>'&defVals[tt_content][CType]=list&defVals[tt_content][list_type]=podcast_display'
-		);
+		);  
+		return $wizardItems;
 	}
 }
 
