86e8ed3a2ec112479a694ba3972cab619d7bc211
Noël Bossart <noel.bossart@namics.com>
2012-06-08 10:34:39 +0200

    [FIXED] Keyword Integration

--- Configuration/TCA/Episode.php
+++ Configuration/TCA/Episode.php
@@ -245,26 +245,28 @@ $TCA['tx_podcast_domain_model_episode'] = array(
 		),
 		'keywords' => array(
 			'exclude' => 0,
-			'label' => 'LLL:EXT:podcast/Resources/Private/Language/locallang_db.xml:tx_podcast_domain_model_episode.keywords',
+			'label' => 'LLL:EXT:podcast/Resources/Private/Language/locallang_db.xml:tx_podcast_domain_model_podcast.keywords',
 			'config' => array(
 				'type' => 'select',
 				'foreign_table' => 'tx_podcast_domain_model_keyword',
 				'MM' => 'tx_podcast_episode_keyword_mm',
+				'renderMode' => 'checkbox',
 				'size' => 10,
 				'autoSizeMax' => 30,
 				'maxitems' => 9999,
 				'multiple' => 0,
 				'wizards' => array(
-					'_PADDING' => 1,
-					'_VERTICAL' => 1,
-					'edit' => array(
+					 '_POSITION' => 'right',
+					 '_PADDING' => 1,
+					 '_VERTICAL' => 0,
+					/*'edit' => array(
 						'type' => 'popup',
 						'title' => 'Edit',
 						'script' => 'wizard_edit.php',
 						'icon' => 'edit2.gif',
 						'popup_onlyOpenIfSelected' => 1,
 						'JSopenParams' => 'height=350,width=580,status=0,menubar=0,scrollbars=1',
-						),
+						),*/
 					'add' => Array(
 						'type' => 'script',
 						'title' => 'Create new',
